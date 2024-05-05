27bc4c96955ee5fe8e4a71b2772c8e95c5112391
adpaoj <apatinoojeda@gmail.com>
2024-05-05 07:57:28 +0200

    Definitivo. Falta Revisión. Más cambios de UI

--- hackupc/src/components/Alumno.js
+++ hackupc/src/components/Alumno.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../Alumno.css';
+import Button from './Button';
 
 const Alumno = () => {
   return(
     <div className='content-container-al'>
       <h2>Alumno</h2>
-      <p class = "texto-blanco"> Clases pendientes: </p>
+      <p class = "texto-blanco"> Clases pendientes: 2/10</p>
+      <Button />
     </div>
   );
 };
